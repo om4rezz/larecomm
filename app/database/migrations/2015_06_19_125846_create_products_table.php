285c70b57ddc3993a8dc4b0198708e1167176ccc
om4rezz <omarezz.developer@gmail.com>
2015-06-19 15:55:08 +0200

    fix the products table migration issue >>>> separate the foreign key

---
+++ app/database/migrations/2015_06_19_125846_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function(Blueprint $table){
+			$table->increments('id');
+			$table->string('title');
+			$table->text('description');
+			$table->decimal('price', 6, 2);
+			$table->boolean('availability')->default(1);
+			$table->string('image');
+			$table->timestamps();
+		});	
+
+		Schema::table('products', function(Blueprint $table){
+			$table->integer('category_id')->after('id')->nullable()->unsigned();
+		});
+		Schema::table('products', function(Blueprint $table){
+			$table->foreign('category_id')->references('id')->on('categories');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('products');
+	}
+
+}
