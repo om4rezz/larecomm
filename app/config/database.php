4305031e617fc7dbce21607c126a0970e33c7b45
xgenOsama <xgen.osama@gmail.com>
2015-07-03 01:51:58 +0200

    signin updated

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'larecomm',
 			'username'  => 'root',
-			'password'  => 'pentester',
+			'password'  => 'g33k',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/views/users/signin.blade.php
+++ app/views/users/signin.blade.php
@@ -8,12 +8,12 @@
         {{ Form::open(array('url'=>'users/signin')) }}
         <p>
             {{ HTML::image('img/email.gif', 'Email Address') }}
-            {{ HTML::text('email') }}
+            {{ Form::text('email') }}
         </p>
 
         <p>
             {{ HTML::image('img/password.gif', 'Password') }}
-            {{ HTML::password('password') }}
+            {{ Form::password('password') }}
         </p>
 
         {{ Form::button('Sign In', array('type'=>'submit', 'class'=>'secondary-cart-btn')) }}
